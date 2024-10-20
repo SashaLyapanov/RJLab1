868dc94c5b516db7f62c8597d547d35147e98a67
SashaLyapanov <lyapanov_s@mail.ru>
2024-10-20 20:52:17 +0300

    Last changes in first lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,13 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-//        Action action = new Action(CompetitionGenerator.generate(5000, 500));
+        Action action = new Action(CompetitionGenerator.generate(5000, 500));
 //        Action action = new Action(CompetitionGenerator.generate(50000, 500));
-        Action action = new Action(CompetitionGenerator.generate(250000, 500));
+//        Action action = new Action(CompetitionGenerator.generate(250000, 500));
         Map res1 = action.simpleLoop();
         Map res2 = action.streamLoop();
         Map res3 = action.customCollectors();
-//        System.out.println("res1");
-//        System.out.println(res1);
-//        System.out.println("res2");
-//        System.out.println(res2);
-//        System.out.println("res3");
-//        System.out.println(res3);
+
         if (res1.equals(res2) && res2.equals(res3)){
             System.out.println("All result is equals!");
         }
